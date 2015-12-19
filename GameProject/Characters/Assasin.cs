818697baa895cea7fd906b1e44e198146c2a6efd
Daniel96Petkov <daniel96petkov@gmail.com>
2015-12-19 08:51:38 +0200

    + Assasin

---
+++ GameProject/Characters/Assasin.cs
@@ -0,0 +1,13 @@
+﻿namespace GameProject.Characters
+{
+    public class Assasin : Character
+    {
+        private new const int AttackDamage = 100;
+        private new const int Health = 200;
+
+        public Assasin(string name) 
+            : base(name, Health, AttackDamage)
+        {
+        }
+    }
+}
