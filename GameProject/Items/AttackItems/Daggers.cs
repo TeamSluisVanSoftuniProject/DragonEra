0107cd4c12a2788bfffd80af2e6caf0bbe29a163
denyslavangelov <denyslavangelov@abv.bg>
2015-12-20 09:09:32 +0200

    Adding Items.

---
+++ GameProject/Items/AttackItems/Daggers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GameProject.Enums;
+
+namespace GameProject.Items.AttackItems
+{
+    public class Daggers : AttackItem
+    {
+        private const int DaggersDamage = 50;
+
+        public Daggers(string name, int price, ItemTypes item) 
+             :base(name, price, item)
+        {
+        }
+
+        public override int Attack
+        {
+            get
+            {
+                switch (this.ItemTypes)
+                {
+                    case ItemTypes.Daggers: return Daggers.DaggersDamage;
+                    default:
+                        return 0;
+                }
+            }
+        }
+    }
+}
---
+++ GameProject/Items/HealthItems/Potion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GameProject.Enums;
+using GameProject.Interfaces;
+using GameStructure.Hero;
+
+namespace GameProject.Items.HealthItems
+{
+    public class Potion : HealthItem
+    {
+        private const int SmallPotionHealing = 30;
+        private const int BigPotionHealing = 60;
+
+        public Potion(string name, int price, ItemTypes item)
+            : base(name, price, item)
+        {
+        }
+
+        public void Heal(Hero hero)
+        {
+            hero.Health += Healing;
+        }
+
+        public override int Healing
+        {
+            get
+            {
+                switch (this.ItemTypes)
+                {
+                    case ItemTypes.SmallPotion:
+                        return Potion.SmallPotionHealing;
+                    case ItemTypes.BigPotion:
+                        return Potion.BigPotionHealing;
+                    default:
+                        return 0;
+                }
+            }
+        }
+    }
+}
