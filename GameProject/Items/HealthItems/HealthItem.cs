7ba6d6cef7bb529ac78730c661996cd095cab720
denyslavangelov <denyslavangelov@abv.bg>
2015-12-20 09:07:35 +0200

    Adding Items class.

---
+++ GameProject/Items/HealthItems/HealthItem.cs
@@ -0,0 +1,15 @@
+﻿using GameProject.Enums;
+
+namespace GameProject.Items.HealthItems
+{
+
+    public abstract class HealthItem : Item
+    {
+        protected HealthItem(string name, int price, ItemTypes item) 
+            :base(name, price, item)
+        {
+        }
+
+        public abstract int Healing { get; }
+    }
+}
