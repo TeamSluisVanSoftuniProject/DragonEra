71978034806c1a4075a022f3b07ddcb60ec3a7cf
Daniel96Petkov <daniel96petkov@gmail.com>
2015-12-20 09:03:51 +0200

    Update Wizard

---
+++ GameProject/Characters/Wizard.cs
@@ -0,0 +1,17 @@
+﻿namespace GameProject.Characters
+{
+    public class Wizard : Character
+    {
+        private const int SpellDamage = 120;
+        private new const int Health = 200;
+        private const int Mana = 200;
+
+        private int mana;
+
+        public Wizard(string name) 
+            : base(name, Health, SpellDamage)
+        {
+            this.mana = Mana;
+        }
+    }
+}
