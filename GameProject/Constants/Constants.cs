b2cd9ca9702ef526229a5c43e948d0e34b42cf12
Daniel96Petkov <daniel96petkov@gmail.com>
2015-12-21 09:00:39 +0200

    2 reda kod

---
+++ GameProject/Constants/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace GameProject.Characters
+{
+    public class Constants
+    {
+        public const int DefaultHealth = 300;
+        public const string CreatureKilled = "You succsesfuly killed ";
+    }
+}
